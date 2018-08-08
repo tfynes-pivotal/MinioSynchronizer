28d658260c87d79fb2a926717e3f355f04ed0bd9
tfynes-pivotal <tmfynes@yahoo.com>
2018-08-08 10:51:25 -0400

    v0.1 Credhub stored s3 secrets

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # MinioSynchronizer
 Deploy an headless app to Cloud Foundry to keep Minio (S3) buckets synchronized.
+
+Application will monitor a source bucket and mirror updates to a target bucket
+
+Credentials are stored in cloud foundry credhub
+
+
+
+
+
+
+Register keys and secrets for source and target buckets in credhub as follows;
+
+    cf create-service credhub default miniocreds -c '{ "Minio1Key": "randomaccesskey", "Minio1Secret":"randomsecretkey", "Minio2Key": "randomaccesskey", "Minio2Secret":"randomsecretkey"}'
+
+
+
+Ensure you set an environment variable for the credhub SI used - CredentialsSIName
+
+
---
+++ go.sh
@@ -0,0 +1,9 @@
+#!/bin/bash 
+      export Minio1Key=$(echo $VCAP_SERVICES | jq -r ".credhub[] | select(.instance_name=\"$CredentialsSIName\").credentials.Minio1Key") \
+ &&   export Minio1Secret=$(echo $VCAP_SERVICES | jq -r ".credhub[] | select(.instance_name=\"$CredentialsSIName\").credentials.Minio1Secret") \
+ &&   export Minio2Key=$(echo $VCAP_SERVICES | jq -r ".credhub[] | select(.instance_name=\"$CredentialsSIName\").credentials.Minio2Key") \
+ &&   export Minio2Secret=$(echo $VCAP_SERVICES | jq -r ".credhub[] | select(.instance_name=\"$CredentialsSIName\").credentials.Minio2Secret") \
+ &&   ./mc config host add minio1 $Minio1Host $Minio1Key $Minio1Secret \
+ &&   ./mc config host add minio2 $Minio2Host $Minio2Key $Minio2Secret \
+ &&   echo READY \
+ &&   ./mc mirror -w minio1/$Minio1Bucket minio2/$Minio2Bucket
